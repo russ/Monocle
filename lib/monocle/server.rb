0477f81a6b80dffd8367b31f419e714d97c002fa
Russ Smith <russ@bashme.org>
2011-08-31 10:00:08 -0700

    Responding as an image to make search engines happier.
    Adding quick README notes for installation.

--- lib/monocle/server.rb
+++ lib/monocle/server.rb
@@ -2,7 +2,7 @@ require "sinatra/base"
 
 module Monocle
   class Server < Sinatra::Base
-    post "/:type/:id" do
+    post "/:type/:id(.:format)" do
       begin
         params[:type].classify.constantize.find(params[:id]).view!
         ActiveRecord::Base.clear_active_connections!
@@ -10,11 +10,6 @@ module Monocle
       rescue ActiveRecord::RecordNotFound
         "o_0"
       end
-    end
-
-    get "/:type/:id" do
-      content_type("image/gif")
-      File.open(File.join(File.dirname(__FILE__), "..", "transparent.gif"))
     end
   end
 end
