8c76d0a250461a524a66094edb740cdaebf86374
Russ Smith <russ@bashme.org>
2012-09-18 11:23:28 -0700

    Matching post and gets and requiring a format to be provided.

--- lib/monocle/server.rb
+++ lib/monocle/server.rb
@@ -2,8 +2,16 @@ require 'sinatra/base'
 
 module Monocle
   class Server < Sinatra::Base
-    post '/:type/:id(.:format)' do
-      if object = params[:type].classify.constantize.find(params[:id])
+    post '/:type/:id.:format' do
+      view_object(params[:type], params[:id])
+    end
+
+    get '/:type/:id.:format' do
+      view_object(params[:type], params[:id])
+    end
+
+    def view_object(type, id)
+      if object = type.classify.constantize.find(id)
         object.view!
         'o_0 +1'
       else
